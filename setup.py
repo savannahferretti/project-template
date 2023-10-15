08f549c14ee414c217a323c4233d6b29cb3a31ff
Savannah Ferretti <savannah.ferretti@uci.edu>
2023-10-15 02:51:58 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name='code',
     packages=find_packages(),
     version='0.1.0',
     description='A template repository for organizing new research projects.',
-    author='Savannah FFerretti',
+    author='Savannah Ferretti',
     license='',
 )
